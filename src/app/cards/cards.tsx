1a915fb2fca3f0506c8b0437c75f272e199cea6f
Jai Sharma <jai@jai.moe>
2023-06-11 13:08:50 +0100

    Add framer-motion Reorder to cards component

--- src/app/cards/cards.tsx
+++ src/app/cards/cards.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import { Reorder } from 'framer-motion'
 import styles from './cards.module.css'
 
 const DEFAULT_CARDS = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -9,12 +10,12 @@ export default function CardsComponent() {
   const [cards, setCards] = useState(DEFAULT_CARDS)
 
   return (
-    <div className={styles.container}>
+    <Reorder.Group className={styles.container} axis="x" values={cards} onReorder={setCards}>
       {cards.map((card) => (
-        <div className={styles.card} key={card}>
+        <Reorder.Item className={styles.card} key={card} value={card}>
           {card}
-        </div>
+        </Reorder.Item>
       ))}
-    </div>
+    </Reorder.Group>
   )
 }
